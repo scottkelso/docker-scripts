8e910c7ea386209121384b40c3182474f0c17751
40129940 <jkelso07@qub.ac.uk>
2018-07-24 10:16:29 +0100

    Make docker-death only remove running containers unless -a is passed in as an arg.

--- docker-death.sh
+++ docker-death.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Use `sh docker-death.sh -a` to remove all containers,
+# including those not running
+
 # Stop all running containers
 docker stop $(docker ps -q)
 
 # Delete all containers
-docker rm $(docker ps -a -q)
+docker rm $(docker ps $1 -q)
